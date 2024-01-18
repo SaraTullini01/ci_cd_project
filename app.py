4a0f6844eccc7485975e397f3a8d64837d09103f
SaraTullini01 <sara.tullini@edu.unife.it>
2024-01-18 17:22:06 +0000

    aggiunta possibilità di visualizzazione su browser

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "<h1>Hello World!</h1><br>Ciao :)<br>"
+    
+    return "<h1>dovresti funzionare!</h1><br><p>non mi deludere!</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+    
